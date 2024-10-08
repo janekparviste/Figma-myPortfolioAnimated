0d9bdca5ffca7210d7fc9241aaf62bf1da9c5898
JanekParviste <janek.parviste@tktk.ee>
2024-10-08 23:23:34 +0300

    js images

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const btnHamburger = document.querySelector('.hamburger');
 const menuPanel = document.querySelector('.menu-panel');
 const menuLinks = document.querySelectorAll('.menu-panel a');
 let close = document.querySelector('.close');
+const frameImage = document.querySelector('#frame');
+const images = ['1.webp', '2.webp', '3.webp', '4.webp', '5.webp', '6.webp', '7.webp'];
+
+let currentImageIndex = 0;
+
+frameImage.addEventListener('click', () => {
+    currentImageIndex++;
+    if(currentImageIndex >= images.length) currentImageIndex = 0;
+    frameImage.src ='images_frame/' + images[currentImageIndex];
+});
 
 close.addEventListener('click', () => {
     menuPanel.classList.remove('active');
